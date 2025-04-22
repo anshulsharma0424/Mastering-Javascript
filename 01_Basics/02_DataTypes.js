bb359e7ada205ee5e143605ef56d7f60ea2c969f
Anshul Sharma <134866369+anshulsharma0424@users.noreply.github.com>
2025-04-22 23:06:31 +0000

    Datatypes in JS

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,55 @@
+"use strict"; // treat all JS code as newer version
+
+// --------------------- Datatypes conversions --------------------- 
+
+// let score = "33"
+// console.log(typeof score)
+
+// let scoreInNumber = Number(score)
+// console.log(typeof scoreInNumber)
+
+// let anotherValue = "abcde"
+// console.log(typeof anotherValue)
+
+// let anotherValueInNumber = Number(anotherValue)
+// console.log(typeof anotherValueInNumber)
+// console.log(anotherValueInNumber)
+
+// let takeNull = null
+// console.log(typeof takeNull)
+
+// let takeNullInNumber = Number(takeNull)
+// console.log(takeNullInNumber)
+
+// let takeUndefined = undefined
+// console.log(typeof takeUndefined)
+// console.log(Number(takeUndefined))
+
+
+// console.log(Number(true), Number(false))
+
+
+
+/*
+------------------------------ NOTES ------------------------------
+
+JavaScript has 8 basic data types:
+I. Primitive Data Types:
+
+- Number: Represents numeric values, including integers and floating-point numbers.
+- String: Represents textual data, enclosed in single or double quotes.
+- Boolean: Represents logical values, true or false.
+- BigInt: Represents integers of arbitrary length.
+- Symbol: Represents unique and immutable identifiers.
+- Null (standalone value, can be assigned to a variable): Represents the intentional absence of a value. | Null is an 'Object'
+- Undefined: Represents a variable that has not been assigned a value.
+
+II. Non-primitive Data Type:
+
+- Object: Represents collections of key-value pairs and can store complex data structures.
+- Arrays: Ordered lists of values.
+- Dates: Represents date and time values.
+- Maps: Collections of key-value pairs where keys can be of any data type.
+- Sets: Collections of unique values.
+
+*/
